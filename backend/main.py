b14fea8d42d47d71bfc3186698eddc2849b066d6
Anteneh Bedilu <anteneh@antenehbedilu.com>
2023-11-18 22:32:02 +0000

    feat(backend/main.py): add health check endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,8 @@ from fastapi import FastAPI
 
 #customize metadata configurations and define URLs for API documentation
 app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi', redoc_url=None, title='Uptime', description='**Uptime** a *RESTful API* to get instant notifications, manage monitors and check your uptime statistics on the go.', summary='Easy-to-use, self-hosted monitoring tool.', version='0.0.1', contact={'name': 'Uptime', 'url': 'https://github.com/antenehbedilu/uptime', 'email': 'hello@antenehbedilu.com'}, license_info={'name': 'MIT License', 'url': 'https://raw.githubusercontent.com/antenehbedilu/uptime/main/LICENSE'})
+
+#endpoint for health check
+@app.get('/api/health', tags=['HealthChecks'])
+def health():
+    return {'status': 'OK'}
